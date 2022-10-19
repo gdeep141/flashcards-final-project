e97513d3cba3fa7f1ee7f3b74840dc7b6c1e05b5
Gauravdeep Gosal <gauravdeep.gosal@capgemini.com>
2022-10-19 18:57:29 +0100

    changed service base url

--- src/components/services/card.js
+++ src/components/services/card.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/flashcards";
+const baseUrl = "http://localhost:3001/api/flashcards";
 
 const getAll = () => {
   return axios.get(baseUrl);
